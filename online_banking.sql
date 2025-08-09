89d192b243d446213e5ba3e7213c9b4381f8754c
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-08-09 12:06:03 +0100

    updating the database

--- online_banking.sql
+++ online_banking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2025 at 10:22 PM
+-- Generation Time: Aug 09, 2025 at 01:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `email`, `password`) VALUES
-(1, 'kingsley', 'arumkingsley49@gmail.com', '1234');
+(1, 'admin', 'admin@admin.com', 'admin@admin.com');
 
 -- --------------------------------------------------------
 
@@ -112,7 +112,7 @@ CREATE TABLE `card` (
 
 INSERT INTO `card` (`id`, `user`, `number`, `date`, `username`, `code`, `status`, `limit`) VALUES
 (3, '1', '4027642035253343', '08/29', 'arum  kingsley', '897', 'dclined', '5000'),
-(4, '9', '1375026131547201', '08/29', 'Rhonda Lee', '897', 'dclined', '5000');
+(4, '9', '1375026131547201', '08/29', 'Rhonda Lee', '897', 'pending', '5000');
 
 -- --------------------------------------------------------
 
@@ -136,9 +136,8 @@ CREATE TABLE `deposit` (
 --
 
 INSERT INTO `deposit` (`id`, `user`, `amount`, `date`, `status`, `payment_type`, `address`, `img`) VALUES
-(1, '1', '30000', '2025-07-30 04:35:56', 'declined', '4', '66666666666666666666666666', 'btmpERVNRM3NvP3BwPt7MUmE5kVjrzCRxsI1SbEaSLHC3FuCYlLSJmTchV4idMs9iuXLtHBGllNpmKR5dhVedTRUapGJk75W1uao_1753842956.png'),
-(2, '1', '100000', '2025-07-30 23:47:15', 'approved', '1', '55555555555555555', 'bFchZkrFavS9A8QnWIhZIQ3cZPB1FF5UJsHH1w47jaEoeOAprKCyFNx0j5qJkJcXt8XeUh0sLgNYytafex6T6eUPReLG2YdRJhGE_1753912035.jpg'),
-(3, '11', '84000', '2025-08-07 13:53:12', 'approved', '4', '66666666666666666666666666', '3s7nqGcJAedEQV26WRg55puk67RoBcUiXasC9Yy86hKGacTT2vBPK5ZIjhz83ARJHrT36EN7IoI7I6ic06T4rQKGAvqc96uCxbcu_1754567592.jpg');
+(1, '4', '30000', '2025-07-30 04:35:56', 'declined', '4', '66666666666666666666666666', 'btmpERVNRM3NvP3BwPt7MUmE5kVjrzCRxsI1SbEaSLHC3FuCYlLSJmTchV4idMs9iuXLtHBGllNpmKR5dhVedTRUapGJk75W1uao_1753842956.png'),
+(2, '1', '100000', '2025-07-30 23:47:15', 'approved', '1', '55555555555555555', 'bFchZkrFavS9A8QnWIhZIQ3cZPB1FF5UJsHH1w47jaEoeOAprKCyFNx0j5qJkJcXt8XeUh0sLgNYytafex6T6eUPReLG2YdRJhGE_1753912035.jpg');
 
 -- --------------------------------------------------------
 
@@ -196,7 +195,7 @@ CREATE TABLE `payment_details_crypto` (
 --
 
 INSERT INTO `payment_details_crypto` (`id`, `name`, `address`, `date`) VALUES
-(1, 'Bitcoin', '5555555555', '2021-10-08 11:05:42'),
+(1, 'Bitcoin', '55555555555555555', '2021-10-08 11:05:42'),
 (2, 'Dodgcoin', '22222222222222222', '2021-11-11 13:26:42'),
 (4, 'Usdt', '66666666666666666666666666', '2021-11-11 13:26:42'),
 (5, 'test', '7777777777777777777777', '2025-07-26 01:39:20');
@@ -232,12 +231,11 @@ CREATE TABLE `transfer` (
 --
 
 INSERT INTO `transfer` (`id`, `amount`, `type`, `receiver_name`, `description`, `date`, `time`, `status`, `receiver_account_number`, `receiver_bank`, `swift_code`, `routine_number`, `country`, `user`, `sender_name`, `receiver_account_type`, `otp_code`) VALUES
-(1, '5500', 'credit', '', '....', '2022-06-04', '01:42', 'approved', '', '', '', '', '', '5', 'mark henry', '', ''),
-(2, '15430', 'credit', '', 'work', '2022-07-21', '23:48', 'approved', '', '', '', '', '', '5', 'Michael James Anderson', '', ''),
-(3, '200', 'credit', '', 'good', '2025-08-06', '14:56:44', 'approved', '7019855552', 'opay', '77777', '565', 'Nigeria', '10', 'kingsley chimezie Ayogu', 'Savings', ''),
-(4, '20000', 'credit', '', 'good', '2025-08-06', '01:12', 'approved', '', '', '', '', '', '11', 'admin', '', ''),
-(5, '200', 'debit', '', 'done deal', '2025-08-06', '13:24', 'approved', '', '', '', '', '', '11', 'kingsley', '', ''),
-(6, '2000000', 'credit', '', 'give me fufu', '2025-08-06', '23:31:12', 'approved', '8766938287431032', 'papa bank', '98989', '767867', 'Algeria', '11', 'kingsley orie akpu 1', 'Current', '');
+(1, '5500', 'credit', '', '....', '2022-06-04', '01:42', 'approved', '', '', '', '', '', '4', 'mark henry', '', ''),
+(2, '15430', 'credit', '', 'work', '2022-07-21', '23:48', 'approved', '', '', '', '', '', '4', 'Michael James Anderson', '', ''),
+(3, '12', 'debit', 'mmicheal', 'ncncncnc', '2025-08-08', '10:14:52', 'cancelled', '84859478395', 'Goldman Sachs', '48jjd', '467378564', 'Argentina', '4', 'mmicheal', 'Fixed Deposit', '815167'),
+(4, '12', 'debit', 'mmicheal', 'ncncncnc', '2025-08-08', '10:18:15', 'pending', '84859478395', 'Goldman Sachs', '48jjd', '467378564', 'Argentina', '4', 'mmicheal', 'Fixed Deposit', '577025'),
+(5, '404', 'debit', 'emeka', 'for school fee', '2025-08-08', '10:48:03', 'cancelled', '748464', 'Ally Bank', '48jjd', '457373', 'Andorra', '4', 'emeka', 'Online Banking', '229752');
 
 -- --------------------------------------------------------
 
@@ -274,24 +272,27 @@ CREATE TABLE `users` (
   `billing_status` varchar(255) DEFAULT 'deactivate',
   `loan_balance` int(11) NOT NULL,
   `kyc` varchar(255) NOT NULL DEFAULT 'unverified',
-  `2_step_verification` tinyint(1) NOT NULL DEFAULT 0
+  `2_step_verification` tinyint(1) NOT NULL DEFAULT 0,
+  `transfer_count` int(11) NOT NULL DEFAULT 0,
+  `status_message` varchar(9000) NOT NULL,
+  `wrong_attempts` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `pin`, `firstname`, `lastname`, `date_created`, `occupation`, `country`, `address`, `gender`, `balance`, `phone`, `ssn`, `profile`, `currency`, `account_number`, `status`, `birth`, `account_type`, `account_limit`, `tax_code`, `cot_code`, `imf_code`, `billing_status`, `loan_balance`, `kyc`, `2_step_verification`) VALUES
-(2, 'arum101', 'arumkingsley9@gmail.com', '$2y$10$/Q3WQSfTeH6C8tRYGs63fezDStVwv2YPRO0aTQnIM7.GGEfPDXYIi', '1111', 'arum ', 'kingsley', '2025-08-04 19:24:18', 'programmer', 'Angola', 'enugu  nigeria obeagu ', 'male', '0', '11111', '1111', 'arumkingsley9_gmail_com_1754335458.png', '$', '2147483647', 'active', '2025-08-04', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(3, 'Gutiérrez', 'mangercello@gmail.com', '$2y$10$3PgIhKpJ6nn6JjbqXFwdeuiSNxt5D4sKLsP/uzOGlhWajZvKPnN9e', '172005', 'William ', 'Levy ', '2025-08-04 19:34:32', 'Actor ', 'Select Country', '2536 Privet Cres Mississauga ON L5B 2S4', 'male', '0', '4174966610', '122331', 'mangercello_gmail_com_1754336072.jpeg', '$', '2147483647', 'active', '1980-08-29', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(4, 'Thebest', 'spotwebdev.com@gmail.com', '$2y$10$8pn42j0PlbG45gbjRoo.ROdCVTanDgcCU694jEONFWhdXYlZMQhVG', '0810', 'Firstclass', 'Thebest', '2025-08-04 21:01:51', 'Web developer ', 'Brunei', 'Mtd gariki ', 'male', '0', '093938734', 'pp', 'spotwebdev_com_gmail_com_1754341312.png', '€', '2147483647', 'active', '2025-07-15', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(5, 'garcia3310', 'martinezgarcia3310@gmail.com', '$2y$10$FgiT0JbuHEzQxoWzJBYeDOWWEwSNNCIOforhnMfRc8NrXsW4msKOm', '3310', 'martinez', 'garcia', '2025-08-04 21:37:01', 'lumberjack supervisor', 'United States of America', 'indiana united state ', 'male', '20930', '7188707384', '1111', 'martinezgarcia3310_gmail_com_1754343421.jpg', '$', '2005902045', 'active', '1962-06-17', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(6, 'Zaynmalik11', 'huntd281@gmail.com', '$2y$10$fp0.FD9RcmWeVN4ubqpDHOHtdSzF6PBiHPP9Z/C2xuLKb21UdRf8e', '1122', 'Zayn', 'Malik', '2025-08-05 09:14:28', 'Musician', 'United Kingdom', '16 Acacia Avenue LONDON EC1Y 8SY UNITED KINGDOM', 'male', '0', '09065802340', '12345', 'huntd281_gmail_com_1754385268.jpeg', '$', '2147483647', 'active', '1998-08-05', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(7, 'Zaynjavaddmalik', 'huntd2006@gmail.com', '$2y$10$e6L4Jux848uZ.Jv0J7GXAuJFEQVJdFnmPGuxFXu5hL1SWqZHUNNZ2', '2016', 'Zayn', 'Javadd Malik ', '2025-08-05 09:18:35', 'Musicians ', 'United States of America', '16 Acacia Avenue LONDON EC1Y 8SY UNITED KINGDOM', 'male', '0', '09130342170', '1111', 'huntd2006_gmail_com_1754385515.jpeg', '$', '2147483647', 'active', '1993-01-12', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(8, 'Zayn', 'Zaynmalik11281@gmail.com', '$2y$10$42SF7DqHE18Xm.6X2ZAsweWtk2tQHCtK3lZ8Ofyp1zSIud1.PLuNm', '1122', 'Zayn', 'Javadd Malik ', '2025-08-05 09:29:30', 'Musician', 'United Kingdom', '16 Acacia Avenue LONDON EC1Y 8SY UNITED KINGDOM', 'male', '0', '08037439264', '1234', 'Zaynmalik11281_gmail_com_1754386170.jpeg', '$', '2147483647', 'active', '1993-01-12', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(9, 'Babygirl25', 'lee52rhonda@gmail.com', '$2y$10$921ayMj6OwjuOifp5PF38Otk4fXwWKeB03emCxEI4sX94OVYFo3om', '4377', 'Rhonda', 'Lee', '2025-08-05 16:58:51', 'Sales', 'United States of America', 'Po Box 127', 'female', '0', '4179430712', '494806967', 'lee52rhonda_gmail_com_1754413131.jpg', '$', '2147483647', 'active', '1967-01-07', 'Online Banking', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(10, 'arum101', 'arumkingsley49@gmail.com', '$2y$10$tCFlz2qM4QXBmKNWB7.YU.zqIYBXKmWZXRXX/5ybIFNzeKlc34QjK', '2006', 'arum ', 'kingsley', '2025-08-05 17:59:57', 'programmer', 'Nigeria', 'enugu  nigeria obeagu ', 'male', '200', '09017862743', '1111', 'arumkingsley49_gmail_com_1754416797.jpg', '€', '5701925111', 'active', '2025-07-29', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0),
-(11, 'Ayogu', 'ayoguchimezie00@gmail.com', '$2y$10$jGG3kN39d1xaEu.4tVQWBOIO2Q.1GXg2zKy7iNDAAKjKWthI5tL/e', '1234', 'Ayogu', 'Chimezie', '2025-08-06 15:08:22', 'banker', 'Nigeria', 'No 19 Enyihuchiri Street', 'male', '2103810', '07019855552', '+244 576 999', 'ayoguchimezie00_gmail_com_1754485702.jpg', '$', '8766938287431032', 'active', '2025-08-06', 'Savings', 5000, '1234', '1234', '1234', 'deactivate', 0, 'verified', 0);
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `pin`, `firstname`, `lastname`, `date_created`, `occupation`, `country`, `address`, `gender`, `balance`, `phone`, `ssn`, `profile`, `currency`, `account_number`, `status`, `birth`, `account_type`, `account_limit`, `tax_code`, `cot_code`, `imf_code`, `billing_status`, `loan_balance`, `kyc`, `2_step_verification`, `transfer_count`, `status_message`, `wrong_attempts`) VALUES
+(2, 'arum101', 'arumkingsley9@gmail.com', '$2y$10$/Q3WQSfTeH6C8tRYGs63fezDStVwv2YPRO0aTQnIM7.GGEfPDXYIi', '1111', 'arum ', 'kingsley', '2025-08-04 19:24:18', 'programmer', 'Angola', 'enugu  nigeria obeagu ', 'male', '0', '11111', '1111', 'arumkingsley9_gmail_com_1754335458.png', '$', '2147483647', 'active', '2025-08-04', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(3, 'Gutiérrez', 'mangercello@gmail.com', '$2y$10$3PgIhKpJ6nn6JjbqXFwdeuiSNxt5D4sKLsP/uzOGlhWajZvKPnN9e', '172005', 'William ', 'Levy ', '2025-08-04 19:34:32', 'Actor ', 'Select Country', '2536 Privet Cres Mississauga ON L5B 2S4', 'male', '0', '4174966610', '122331', 'mangercello_gmail_com_1754336072.jpeg', '$', '2147483647', 'active', '1980-08-29', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(4, 'Thebest', 'spotwebdev.com@gmail.com', '$2y$10$8pn42j0PlbG45gbjRoo.ROdCVTanDgcCU694jEONFWhdXYlZMQhVG', '0810', 'Firstclass', 'Thebest', '2025-08-04 21:01:51', 'Web developer ', 'Brunei', 'Mtd gariki ', 'male', '3773', '093938734', 'pp', 'spotwebdev_com_gmail_com_1754341312.jpg', '€', '2147483647', 'suspended', '2025-07-15', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, 'Your account has been suspended after 3 incorrect PIN attempts.', 3),
+(5, 'garcia3310', 'martinezgarcia3310@gmail.com', '$2y$10$FgiT0JbuHEzQxoWzJBYeDOWWEwSNNCIOforhnMfRc8NrXsW4msKOm', '3310', 'martinez', 'garcia', '2025-08-04 21:37:01', 'lumberjack supervisor', 'United States of America', 'indiana united state ', 'male', '20930', '7188707384', '1111', 'martinezgarcia3310_gmail_com_1754343421.jpg', '$', '2005902045', 'active', '1962-06-17', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(6, 'Zaynmalik11', 'huntd281@gmail.com', '$2y$10$fp0.FD9RcmWeVN4ubqpDHOHtdSzF6PBiHPP9Z/C2xuLKb21UdRf8e', '1122', 'Zayn', 'Malik', '2025-08-05 09:14:28', 'Musician', 'United Kingdom', '16 Acacia Avenue LONDON EC1Y 8SY UNITED KINGDOM', 'male', '0', '09065802340', '12345', 'huntd281_gmail_com_1754385268.jpeg', '$', '2147483647', 'active', '1998-08-05', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(7, 'Zaynjavaddmalik', 'huntd2006@gmail.com', '$2y$10$e6L4Jux848uZ.Jv0J7GXAuJFEQVJdFnmPGuxFXu5hL1SWqZHUNNZ2', '2016', 'Zayn', 'Javadd Malik ', '2025-08-05 09:18:35', 'Musicians ', 'United States of America', '16 Acacia Avenue LONDON EC1Y 8SY UNITED KINGDOM', 'male', '0', '09130342170', '1111', 'huntd2006_gmail_com_1754385515.jpeg', '$', '2147483647', 'active', '1993-01-12', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(8, 'Zayn', 'Zaynmalik11281@gmail.com', '$2y$10$42SF7DqHE18Xm.6X2ZAsweWtk2tQHCtK3lZ8Ofyp1zSIud1.PLuNm', '1122', 'Zayn', 'Javadd Malik ', '2025-08-05 09:29:30', 'Musician', 'United Kingdom', '16 Acacia Avenue LONDON EC1Y 8SY UNITED KINGDOM', 'male', '0', '08037439264', '1234', 'Zaynmalik11281_gmail_com_1754386170.jpeg', '$', '2147483647', 'active', '1993-01-12', 'Current', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(9, 'Babygirl25', 'lee52rhonda@gmail.com', '$2y$10$921ayMj6OwjuOifp5PF38Otk4fXwWKeB03emCxEI4sX94OVYFo3om', '4377', 'Rhonda', 'Lee', '2025-08-05 16:58:51', 'Sales', 'United States of America', 'Po Box 127', 'female', '0', '4179430712', '494806967', 'lee52rhonda_gmail_com_1754413131.jpg', '$', '2147483647', 'active', '1967-01-07', 'Online Banking', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(10, 'arum101', 'arumkingsley49@gmail.com', '$2y$10$tCFlz2qM4QXBmKNWB7.YU.zqIYBXKmWZXRXX/5ybIFNzeKlc34QjK', '2006', 'arum ', 'kingsley', '2025-08-05 17:59:57', 'programmer', 'Nigeria', 'enugu  nigeria obeagu ', 'male', '0', '09017862743', '1111', 'arumkingsley49_gmail_com_1754416797.jpg', '€', '5701925111', 'active', '2025-07-29', 'Savings', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0),
+(11, '', 'spotwebdev1.com@gmail.com', '$2y$10$0vwXRv8mU8/W9OAch3TvNeOQgZjSnFuvo/ZcOFYBfEh9gaQGfw7da', '0000', 'Ezea', 'Ugochukwu', '2025-08-09 08:17:14', '', 'Nigeria', '', '', '0', '', 'pp', 'spotwebdev1_com_gmail_com_1754720234.jpg', 'Fr', '2196722921', 'active', '2025-08-05', 'Fixed Deposit', 5000, '', '', '', 'deactivate', 0, 'unverified', 0, 0, '', 0);
 
 -- --------------------------------------------------------
 
@@ -400,7 +401,7 @@ ALTER TABLE `card`
 -- AUTO_INCREMENT for table `deposit`
 --
 ALTER TABLE `deposit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `loan`
@@ -424,7 +425,7 @@ ALTER TABLE `payment_details_crypto`
 -- AUTO_INCREMENT for table `transfer`
 --
 ALTER TABLE `transfer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
