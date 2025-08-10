900b1515f7823a3336efd2bd8cf602f3ab1237e2
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-08-10 13:51:57 +0100

    added live support

--- app/includes/nav.php
+++ app/includes/nav.php
@@ -199,3 +199,18 @@
 
 
     
+
+
+
+    <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b2ac0652e04a62a26f9b93dc4dc349281c33e5bf';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
