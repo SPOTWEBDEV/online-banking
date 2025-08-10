4e9b5b1131b8c4df7bd2b226534c2e2f6679a449
kingsley <ayoguchimezie00@gmail.com>
2025-08-10 17:52:34 +0000

    done

--- .history/admin/includes/sidebar_20250810171304.php
+++ .history/admin/includes/sidebar_20250810171304.php
@@ -103,7 +103,7 @@
                         <a href="<?php echo $domain . "admin/users/kyc.php"  ?>"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Kyc </a>
                     </li>
                       <li>
-                        <a href="<?php echo $domain . "admin/users/contact_us.php"  ?>"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span>  contacts/message</a>
+                        <a href="<?php echo $domain . "admin/users/contact_us.php"  ?>"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span>  contacts/messages </a>
                     </li>
                 </ul>
             </div>
